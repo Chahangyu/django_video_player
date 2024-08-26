a3ae3f50a12f35f174e83f8eed6271ccc71b1a0f
Chahangyu <kja041108@gmail.com>
2024-08-26 22:28:12 +0900

    update

--- video_project/settings.py
+++ video_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,14 +117,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# 기존 설정
+STATIC_URL = '/static/'
+
+# 추가: 프로젝트 전역에서 사용하는 정적 파일 경로 설정
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# 배포 시 사용할 정적 파일 경로 설정
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import os
 
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
